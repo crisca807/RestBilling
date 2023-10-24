d9207c6f90107e1119cc1dc1773fe6fe0316d539
Aprendiz <aprendizsena@soy.sena.edu.co>
2023-10-24 15:40:54 -0500

    conf(git):add .gitignore and .gitattibute

--- RestBilling/RestBilling/obj/Debug/net7.0/RestBilling.AssemblyInfo.cs
+++ RestBilling/RestBilling/obj/Debug/net7.0/RestBilling.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("35d3ba9f-4b41-4edd-a3b0-734882989050")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestBilling")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
